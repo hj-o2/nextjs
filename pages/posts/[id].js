d3a73a81fafb6adbd3248e252d34ce5b10fa6010
hj-yuki <hjystfc07gd2020@gmail.com>
2022-07-05 16:31:36 +0900

    init

---
+++ pages/posts/[id].js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { getAllPostsIds, getPostData } from "../../lib/post";
+import utilStyle from "../../styles/utils.module.css";
+
+export async function getStaticPaths() {
+  const paths = getAllPostsIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyle.headingXl}>{postData.title}</h1>
+        <div className={utilStyle.lightText}>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.blogContentHTML }} />
+      </article>
+    </Layout>
+  );
+}
